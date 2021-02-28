854e695b89f428288fbce51619fc0c21a112465c
Brendon Andrade <brendon.andrade@softplan.com.br>
2021-02-28 19:01:51 -0300

    Finalização do Exemplo

---
+++ digionebank/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteCliente.kt
@@ -0,0 +1,12 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Cliente
+import one.digitalinnovation.digionebank.ClienteTipo
+
+fun main() {
+    val jose = Cliente(nome = "José da Silva", cpf = "1234456777", clienteTipo = ClienteTipo.PF, senha = "123456")
+
+    println(jose)
+
+    TesteAutenticacao().autentica(jose)
+}
