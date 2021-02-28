38a02ea57732de17d97edf40927c80376597b387
Brendon Andrade <brendon.andrade@softplan.com.br>
2021-02-28 11:43:49 -0300

    Atualização para classes abstratas

--- digionebank/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ digionebank/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,18 +1,7 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "Brendon"
-
-    var cpf: String = "123.456.789-00"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-}
-
-fun main(){
-    val brendon = Pessoa()
-
-    println(brendon.pessoaInfo())
+abstract class Pessoa(
+    val nome: String,
+    val cpf: String
+) {
 }
